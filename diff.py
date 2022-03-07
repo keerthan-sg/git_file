9b50d0d0c6aca3fe9458ebe4727e9d439abdb7ee
unknown <keerthansp11@gmail.com>
2022-03-07 16:11:13 +0530

    added implementation for diff.py

--- diff.py
+++ diff.py
@@ -6,4 +6,4 @@ num2 = input('Enter second number: ')
 difference = float(num1) - float(num2)
 
 # Display the difference
-print('The difference of {0} and {1} is {2}'.format(num1, num2, difference))
+print("difference=",difference)
